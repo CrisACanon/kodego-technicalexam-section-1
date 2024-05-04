635443070f3e6f55cdc002f078e0f99488e143e3
CrisACanon <mrcris.canon@gmail.com>
2024-05-04 18:28:19 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,8 @@
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "now that your near.jpg",
+    src: "Now That You're Near - Hillsong Worship.mp3",
+  },
+];
